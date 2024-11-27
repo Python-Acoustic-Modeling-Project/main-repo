2739f202e8043b4b1e62160ad37735b450576e23
Pjark Sander <pjark.sander@gmail.com>
2024-11-27 15:34:06 -0500

    moved everything into a class to make usage easier

--- view.py
+++ view.py
@@ -4,27 +4,20 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import os
 
-rt60 = 0.0
-filelength = 0.0
-rt_difference = 0.0
-resonant_frequency = 0.0
-filename = ""
-graph_types = ["Intensity Graph", "Waveform Graph", "RT60 Low", "RT60 Medium", "RT60 High", "RT60 Combined"]
-
-def select_path(entry_field):
+'''def select_path(entry_field):
     path = filedialog.askopenfilename()
     entry_field.delete(0, tk.END)
     entry_field.insert(0, path)
     filename = find_filename(path)
-    label_name.config(text=("   File: " + filename))
+    label_name.config(text=("   File: " + filename))'''
 
-def get_path():
-    return path
+'''def get_path():
+    return path'''
 
-def find_filename(path):
-    return os.path.basename(path).split('/')[-1]
+'''def find_filename(path):
+    return os.path.basename(path).split('/')[-1]'''
 
-def change_graph():
+'''def change_graph():
     graph_disp = graph_select_var.get()
     if graph_disp == "Intensity Graph":
         pass
@@ -36,46 +29,90 @@ def change_graph():
         pass
     elif graph_disp == "RT60 High":
         pass
+'''
 
-root = tk.Tk()
-root.title("SPIDAM")
-root.grid()
 
-root.geometry("800x800")
+class view:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("SPIDAM")
+        self.root.grid()
+        self.root.geometry("800x800")
 
-label = tk.Label(root, text="")
-label.grid(column=0, row=0)
+        #initialize data variables
+        self.rt60 = 0.0
+        self.filelength = 0.0
+        self.rt_difference = 0.0
+        self.resonant_frequency = 0.0
+        self.filename = ""
+        self.location = ""
+        self.graph_types = ["Intensity Graph", "Waveform Graph", "RT60 Low", "RT60 Medium", "RT60 High", "RT60 Combined"]
 
-label = tk.Label(root, text="   File entry:")
-label.grid(column=0, row=1)
+        #Labels
+        self.label = tk.Label(self.root, text="   File entry:")
+        self.label.grid(column=0, row=1)
 
-path = tk.Entry(root, width=50)
-path.grid(column=1, row=1, columnspan=3)
+        #
+        self.path = tk.Entry(self.root, width=50)
+        self.path.grid(column=1, row=1, columnspan=3)
 
-file_button = tk.Button(root, text="Choose a file", command=lambda: select_path(path))
-file_button.grid(column=4, row=1)
+        #
+        self.file_button = tk.Button(self.root, text="Choose a file", command=lambda: select_path())
+        self.file_button.grid(column=4, row=1)
 
-label_name = tk.Label(root, text=("   File: " + filename))
-label_name.grid(column=0, row=2, columnspan=4, sticky=('W'))
+        self.label_name = tk.Label(self.root, text=("   File: " + self.filename))
+        self.label_name.grid(column=0, row=2, columnspan=4, sticky=('W'))
 
-graph_select_var = tk.StringVar(value=graph_types[0])
-graph_select = tk.OptionMenu(root, graph_select_var, *graph_types, command=change_graph)
-graph_select.grid(column=1, row=3, columnspan=4, sticky=('W'), pady = 10)
+        self.graph_select_var = tk.StringVar(value=self.graph_types[0])
+        self.graph_select = tk.OptionMenu(self.root, self.graph_select_var, *self.graph_types, command=self.change_graph())
+        self.graph_select.grid(column=1, row=3, columnspan=4, sticky=('W'), pady = 10)
 
-fig,ax = plt.subplots()
-canvas = FigureCanvasTkAgg(fig,root)
-canvas.draw()
-widget = canvas.get_tk_widget()
-widget.grid(column=1, row=4, columnspan=4, sticky=('W'))
+        self.fig,self.ax = plt.subplots()
+        self.canvas = FigureCanvasTkAgg(self.fig,self.root)
+        self.canvas.draw()
+        self.widget = self.canvas.get_tk_widget()
+        self.widget.grid(column=1, row=4, columnspan=4, sticky=('W'))
 
-label_file_length = tk.Label(root, text=("File Length: " + str(filelength) + "s"))
-label_file_length.grid(column=1, row=5, columnspan=4, sticky=('W'))
+        #Label to display file length
+        self.label_file_length = tk.Label(self.root, text=("File Length: " + str(self.filelength) + "s"))
+        self.label_file_length.grid(column=1, row=5, columnspan=4, sticky=('W'))
 
-label_file_Resonant_Frequency = tk.Label(root, text=("Resonant Frequency: " + str(resonant_frequency) + "Hz"))
-label_file_Resonant_Frequency.grid(column=1, row=6, columnspan=4, sticky=('W'))
+        #Label to display resonant frequency
+        self.label_file_Resonant_Frequency = tk.Label(self.root, text=("Resonant Frequency: " + str(self.resonant_frequency) + "Hz"))
+        self.label_file_Resonant_Frequency.grid(column=1, row=6, columnspan=4, sticky=('W'))
 
-label_file_rt60_diff = tk.Label(root, text=("RT60 Difference: " + str(rt_difference) + "s"))
-label_file_rt60_diff.grid(column=1, row=7, columnspan=4, sticky=('W'))
+        #Label to display RT60 Difference
+        self.label_file_rt60_diff = tk.Label(self.root, text=("RT60 Difference: " + str(self.rt_difference) + "s"))
+        self.label_file_rt60_diff.grid(column=1, row=7, columnspan=4, sticky='W')
+        self.root.mainloop()
 
-root.mainloop()
+    def select_path(self):
+        self.location = filedialog.askopenfilename()
+        self.path.delete(0, tk.END)
+        self.path.insert(0, self.location )
+        self.filename = self.find_filename()
+        self.label_name.config(text=("   File: " + self.filename))
+
+    def get_path(self):
+        return self.path
+
+    def find_filename(self):
+        return os.path.basename(self.location ).split('/')[-1]
+
+    def change_graph(self):
+        graph_disp = self.graph_select_var.get()
+        if graph_disp == "Intensity Graph":
+            pass
+        elif graph_disp == "Waveform Graph":
+            pass
+        elif graph_disp == "RT60 Low":
+            pass
+        elif graph_disp == "RT60 Medium":
+            pass
+        elif graph_disp == "RT60 High":
+            pass
+
+hello = view()
+
+
 
