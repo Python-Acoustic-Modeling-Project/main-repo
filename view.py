33456e2769d75bb9a0277471a02938ba290d25fe
Brady Kondek <34287058+bradykondek@users.noreply.github.com>
2024-12-02 23:39:24 -0500

    Minor touch-ups, and add more comments throughout modules (#14)

--- view.py
+++ view.py
@@ -16,17 +16,16 @@ class View:
         """
 
         # Set instance variables
-        self.root = tk.Tk()
-        self.root.minsize(800, 800)
-        self.root.title("SPIDAM Audio Analysis Tool")
-        self.controller = controller
-        self.is_playing = False
-        self.index = 0
-        self.index = 0
+        self.root = tk.Tk()  # create Tkinter instance
+        self.root.minsize(800, 800)  # represents the size of the application window
+        self.root.title("SPIDAM Audio Analysis Tool")  # set application title
+        self.controller = controller  # assign controller to instance variable
+        self.is_playing = False  # boolean variable to represent if audio is currently playing
+        self.index = 0  # integer variable to represent the current index for the RT60 cycle graphs
         self.file_imported = False  # keeps track of if audio file has been imported yet
         self.file_analyzed = False  # keeps track of if audio file has been analyzed yet
 
-        # Title
+        # Title heading at the top of application window
         tk.Label(self.root, text="SPIDAM Audio Analysis Tool", font=("Arial", 20, "bold")).pack(anchor=tk.CENTER, pady=10)
 
         # File import status text
@@ -49,69 +48,77 @@ class View:
         tk.Label(self.root, text="Visualizations", font=("Arial", 16, "bold")).pack(anchor=tk.CENTER, pady = 10)
 
         # Tabs for data display
-        self.tabControl = ttk.Notebook(self.root)
-        self.tab1 = ttk.Frame(self.tabControl)
-        self.tab2 = ttk.Frame(self.tabControl)
-        self.tab6 = ttk.Frame(self.tabControl)
+        self.tabControl = ttk.Notebook(self.root)  # sets up "container" for tabs, and the content within each one
+        self.tab1 = ttk.Frame(self.tabControl)  # set up tab 1, for waveform visualization graph
+        self.tab2 = ttk.Frame(self.tabControl)  # set up tab 2, for RT60 graphs
+        self.tab3 = ttk.Frame(self.tabControl)  # set up tab 3, for intensity graph
 
-        self.tabControl.add(self.tab1, text='Waveform')
-        self.tabControl.add(self.tab2, text='RT60 Cycle Graphs')
-        self.tabControl.add(self.tab6, text='Intensity Graph')
-
+        # Add titles to tabs
+        self.tabControl.add(self.tab1, text='Waveform')  # add title to tab 1
+        self.tabControl.add(self.tab2, text='RT60 Cycle Graphs')  # add title to tab 2
+        self.tabControl.add(self.tab3, text='Intensity Graph')  # add title to tab 3
+        
+        # Place in tab figure
         self.tabControl.pack()
 
-        self.fig, self.ax = plt.subplots(figsize=(9, 4))
-        self.ax.set_title("Audio Data Visualization")
-        self.ax.set_xlabel("Time: Seconds")
-        self.ax.set_ylabel("Amplitude")
-        self.canvas = FigureCanvasTkAgg(self.fig, master=self.tab1)
+        # Visualization selection: Waveform graph (tab 1)
+        self.fig, self.ax = plt.subplots(figsize=(9, 4))  # establish plot, and set size
+        self.ax.set_title("Audio Data Visualization")  # set plot title
+        self.ax.set_xlabel("Time: Seconds")  # set x-axis label
+        self.ax.set_ylabel("Amplitude")  # set y-axis label
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.tab1)  # establish canvas to contain plot
         self.canvas_widget = self.canvas.get_tk_widget()
         self.canvas_widget.grid(row = 1, column = 1)
         self.canvas_widget.pack()
 
-        # Visualization selection: RT60 Cycle graph
-        self.fig1, self.ax1 = plt.subplots(figsize=(9, 4))
-        self.ax1.set_title("RT60")
-        self.ax1.set_xlabel("Time: Seconds")
-        self.ax1.set_ylabel("Power: dB")
-        self.canvas1 = FigureCanvasTkAgg(self.fig1, master=self.tab2)
+        # Visualization selection: RT60 Cycle graph (tab 2)
+        self.fig1, self.ax1 = plt.subplots(figsize=(9, 4))  # establish plot, and set size
+        self.ax1.set_title("RT60")  # set plot title
+        self.ax1.set_xlabel("Time: Seconds")  # set x-axis title
+        self.ax1.set_ylabel("Power: dB")  # set y-axis title
+        self.canvas1 = FigureCanvasTkAgg(self.fig1, master=self.tab2)  # establish canvas to contain plot
         self.canvas_widget1 = self.canvas1.get_tk_widget()
         self.canvas_widget1.grid(row = 0, column = 0, columnspan = 6)
 
-        self.rt60_button = tk.Button(self.tab2, text="Cycle RT60 Graphs", command=self.cycle_rt60, font=("Arial", 10))
+        # Cycle RT60 graphs button (within tab 2)
+        self.rt60_button = tk.Button(self.tab2, text="Cycle RT60 Graphs", command=self.cycle_rt60, font=("Arial", 10))  # calls cycle_rt60() function when clicked on
         self.rt60_button.grid(row = 1, column = 2, pady = 10)
 
-        # Combined RT60 graph button
-        self.rt60_combined_button = tk.Button(self.tab2, text="Combined RT60 Graph", command=self.update_combined_rt60, font=("Arial", 10))
+        # Combined RT60 graph button (within tab 2)
+        self.rt60_combined_button = tk.Button(self.tab2, text="Combined RT60 Graph", command=self.update_combined_rt60, font=("Arial", 10))  # calls update_combined_rt60() function when clicked on
         self.rt60_combined_button.grid(row = 1, column = 3, pady = 10)
 
-        # Visualization selection: Intensity
-        self.fig5, self.ax5 = plt.subplots(figsize=(9, 4))
-        self.ax5.set_title("Intensity")
-        self.ax5.set_xlabel("Time: Seconds")
-        self.ax5.set_ylabel("Frequency: Hertz")
-        self.canvas5 = FigureCanvasTkAgg(self.fig5, master=self.tab6)
+        # Visualization selection: Intensity (tab 3)
+        self.fig5, self.ax5 = plt.subplots(figsize=(9, 4))  # establish plot, and set size
+        self.ax5.set_title("Intensity")  # set plot title
+        self.ax5.set_xlabel("Time: Seconds")  # set x-axis title
+        self.ax5.set_ylabel("Frequency: Hertz")  # set y-axis title
+        self.canvas5 = FigureCanvasTkAgg(self.fig5, master=self.tab3)  # establish canvas to contain plot
         self.canvas_widget5 = self.canvas5.get_tk_widget()
         self.canvas_widget5.pack()
 
         # Analysis Results section
-        self.results_frame = tk.Frame(self.root)
+        self.results_frame = tk.Frame(self.root)  # establish frame to contain result labels (that has each result)
         self.results_frame.pack(anchor=tk.CENTER,pady=10)
 
+        # Audio length label
         self.length_label = tk.Label(self.results_frame, text="Length: --- seconds", font=("Arial", 12))
         self.length_label.grid(row=0, column=0, padx=10)
 
+        # RT60 label
         self.rt60_label = tk.Label(self.results_frame, text="RT60: --- seconds", font=("Arial", 12))
         self.rt60_label.grid(row=0, column=1, padx=10)
 
+        # RT60 0.5 difference label
         self.difference_label = tk.Label(self.results_frame, text="RT60 .05 Difference: --- seconds", font=("Arial", 12))
         self.difference_label.grid(row=0, column=2, padx=10)
 
+        # Resonant frequency label
         self.resonant_label = tk.Label(self.results_frame, text="Resonant Frequency: --- Hz", font=("Arial", 12))
         self.resonant_label.grid(row=0, column=3, padx=10)
 
         # Play/stop button
-        self.play_button = tk.Button(self.root, text="Play", command=self.toggle_play, font=("Arial", 12))
+        self.play_button = tk.Button(self.root, text="Play", command=self.toggle_play, font=("Arial", 12))  # calls toggle_play() function when clicked on
         self.play_button.pack(pady=10)
 
         # Status bar
@@ -129,6 +136,7 @@ class View:
         # Initialize pygame mixer
         pygame.mixer.init()
 
+        # Run application
         self.root.mainloop()
 
     # Update status message
@@ -146,9 +154,11 @@ class View:
         Handles the file import process. Calls the controller to load the file and updates the view.
         """
 
-        filepath = filedialog.askopenfilename(filetypes=[("Audio Files", "*.wav *.mp3 *.m4a"), ("All Files", "*.*")])
+        # Trigger file selection window for user to select audio file
+        filepath = filedialog.askopenfilename(filetypes=[("Audio Files", "*.wav *.mp3"), ("All Files", "*.*")])
 
-        if filepath:
+        # If user selects file, load in the file, convert if needed, and analyze it
+        if filepath:  # if filepath exists, meaning user selected proper file
 
             try:  # attempt to do
 
@@ -211,6 +221,7 @@ class View:
         Handles data cleaning via the controller.
         """
 
+        # Analyze audio file, get results, and plot them to respective graphs
         try:  # attempt to do
             
             # Set boolean check for analyzing audio to false (if not already; in case new audio is imported)
@@ -220,13 +231,15 @@ class View:
             self.update_status("Analyzing audio file...")
             
             # Analyze audio and update results
-            self.controller.clean_data()
-            self.results = self.controller.analyze_data()
-            self.update_results(self.results)
-            self.update_visualization(self.results["waveform"])
-            self.cycle_rt60()
-            self.update_combined_rt60()
-            self.update_intensity()
+            self.controller.clean_data()  # clean data
+            self.results = self.controller.analyze_data()  # analyze data and assign results to instance variable
+            self.update_results(self.results)  # update the results from instance variable
+
+            # Prepare/update graphs
+            self.update_visualization(self.results["waveform"])  # update waveform visualization graph
+            self.cycle_rt60()  # update RT60 cycle graphs (low, mid, and high)
+            self.update_combined_rt60()  # update RT60 combined graph
+            self.update_intensity()  # update intensity graph
 
             # Set boolean check for analyzing audio to true
             self.file_analyzed = True
@@ -244,9 +257,16 @@ class View:
         Updates the displayes analysis results.
         """
 
+        # Label to display length of audio file
         self.length_label.config(text=f"Length: {results['length']:.2f} seconds")
+
+        # Label to display RT60
         self.rt60_label.config(text=f"RT60: {results['rt60']:.2f} seconds")
+
+        # Label to display RT60 .5 difference
         self.difference_label.config(text=f"RT60 .5 Difference: {results['rt60'] - 0.5:.2f} seconds")
+
+        # Label to display resonant frequency
         self.resonant_label.config(text=f"Resonant Frequency: {results['resonant_frequency']:.2f} Hz")
 
     # Update visualization
@@ -255,14 +275,24 @@ class View:
         Updates the waveform visualization.
         """
 
+        # Clear current plot
         self.ax.clear()
+
+        # Plot waveform
         self.ax.plot(waveform, label="Waveform")
+
+        # Add legend to plot
         self.ax.legend()
+
+        # Set plot and axis titles
         self.ax.set_title("Audio Waveform")
         self.ax.set_xlabel("Time: Seconds")
         self.ax.set_ylabel("Amplitude")
-        self.canvas.draw()
 
+        # Create display canvas for plot
+        self.canvas.draw()
+    
+    # Check on current audio status, and display respective state in status bar
     def audio_status(self):
 
         # Update status message indicating that graph cannot yet be generated since audio has not been imported
@@ -281,6 +311,8 @@ class View:
         """
         This function updates the combined RT60 graph.
         """
+        
+        # Clear current plot
         self.ax1.clear()
 
         # Update plot title and axis titles
@@ -291,23 +323,24 @@ class View:
         # Separate RT60 results into low, mid, and high; and then plot those results
         try:  # atttempt to do
             
-            rt60_low = self.results["rt60_low"]
-            rt60_mid = self.results["rt60_mid"]
-            rt60_high = self.results["rt60_high"]
+            # Separate RT60 results into low, mid, and high
+            rt60_low = self.results["rt60_low"]  # RT60 low
+            rt60_mid = self.results["rt60_mid"]  # RT60 mid
+            rt60_high = self.results["rt60_high"]  # RT60 high
 
-            # RT60 low
+            # Plot RT60 low
             self.ax1.plot(rt60_low[0], rt60_low[1], linewidth=1, alpha=0.7)
             self.ax1.plot(rt60_low[2], rt60_low[5], 'ro')
             self.ax1.plot(rt60_low[3], rt60_low[6], 'yo')
             self.ax1.plot(rt60_low[4], rt60_low[7], 'go')
 
-            # RT60 mid
+            # Plot RT60 mid
             self.ax1.plot(rt60_mid[0], rt60_mid[1], linewidth=1, alpha=0.7)
             self.ax1.plot(rt60_mid[2], rt60_mid[5], 'ro')
             self.ax1.plot(rt60_mid[3], rt60_mid[6], 'yo')
             self.ax1.plot(rt60_mid[4], rt60_mid[7], 'go')
 
-            # RT60 high
+            # Plot RT60 high
             self.ax1.plot(rt60_high[0], rt60_high[1], linewidth=1, alpha=0.7)
             self.ax1.plot(rt60_high[2], rt60_high[5], 'ro')
             self.ax1.plot(rt60_high[3], rt60_high[6], 'yo')
@@ -336,42 +369,62 @@ class View:
             self.ax1.set_xlabel("Time: Seconds")
             self.ax1.set_ylabel("Power: dB")
 
-            # Determine which rt60 plot to show
-            if self.index%3 == 1:
-                self.ax1.set_title("RT60 Low")
-                rt60_data = self.results["rt60_low"]
-            elif self.index%3 == 2:
-                self.ax1.set_title("RT60 Mid")
-                rt60_data = self.results["rt60_mid"]
-            elif self.index%3 == 0:
-                self.ax1.set_title("RT60 High")
-                rt60_data = self.results["rt60_high"]
+            # Determine which rt60 plot to show (depending on current cycle index; index changes each time Cycle RT60 Graphs button is clicked)
+            if self.index%3 == 1:  # if current cycle index is at 1
+                
+                # Plot RT60 low results
+                self.ax1.set_title("RT60 Low")  # set graph title
+                rt60_data = self.results["rt60_low"]  # update graph with RT60 low data
 
-            #update the graph with the corresponding data
-            if rt60_data:
+            elif self.index%3 == 2:  # if current cycle index is at 2
+
+                # Plot RT60 mid results
+                self.ax1.set_title("RT60 Mid")  # set graph title
+                rt60_data = self.results["rt60_mid"]  # update graph with RT60 mid data
+
+            elif self.index%3 == 0:  # if current cycle index is at 0
+
+                # Plot RT60 high results
+                self.ax1.set_title("RT60 High")  # set graph title
+                rt60_data = self.results["rt60_high"]  # update graph with RT60 high data
+
+            # Update the graph with the corresponding data
+            if rt60_data:  # if RT60 data exists
+
+                # Plot RT60 data
                 self.ax1.plot(rt60_data[0], rt60_data[1], linewidth=1, alpha=0.7, color='#004bc6')
                 self.ax1.plot(rt60_data[2], rt60_data[5], 'ro')  # Plot points for RT60 data
                 self.ax1.plot(rt60_data[3], rt60_data[6], 'go')
                 self.ax1.plot(rt60_data[4], rt60_data[7], 'yo')
 
+            # Draw the plot
             self.canvas1.draw()
 
-            #Update the index by one and keep it in the 1, 2, 3 scheme
+            # Update the index by one and keep it in the 1, 2, 3 scheme
             self.index += 1
             self.index = self.index%3
 
-        except:
+        except Exception:  # except if an exception arises
             
             # Print status message to status bar depending on current state of audio importing/analyzing
             self.audio_status()
 
+    # Update intensity graph
     def update_intensity(self):
         """
         Updates the intensity graphs.
         """
+        
+        # Clear current graph
         self.ax5.clear()
+
+        # Set plot and axis titles
         self.ax5.set_title("Intensity")
         self.ax5.set_xlabel("Time: Seconds")
         self.ax5.set_ylabel("Frequency: Hz")
+
+        # Plot results
         plt.specgram(self.results["Intensity"][0], Fs=self.results["Intensity"][1], NFFT=1024, cmap=plt.get_cmap('autumn_r'))
+
+        # Draw the plot
         self.canvas5.draw()
